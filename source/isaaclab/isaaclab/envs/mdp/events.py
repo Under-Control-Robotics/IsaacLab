66ffa195aefbcb36088ca95f1d1d129052ec6198
Burak Mert Gonultas <gonultasbu@users.noreply.github.com>
2025-10-22 16:31:38 -0700

    change visualization colors

--- source/isaaclab/isaaclab/envs/mdp/events.py
+++ source/isaaclab/isaaclab/envs/mdp/events.py
@@ -45,7 +45,7 @@ force_marker_cfg = VisualizationMarkersCfg(
         "force_arrow": sim_utils.UsdFileCfg(
             usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/UIElements/arrow_x.usd",
             scale=(1.0, 0.1, 0.1),
-            visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(1.0, 0.0, 0.0)),
+            visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=((1.0, 0.984, 0.0))),
         )
     },
 )
